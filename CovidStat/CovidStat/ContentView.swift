838dcad8d2c528af05a3fa287f4d681a109173b4
Amory <amoryrouault@gmail.com>
2020-04-27 21:29:12 +0200

    First commit

---
+++ CovidStat/CovidStat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidStat
+//
+//  Created by Amory Rouault on 27/04/2020.
+//  Copyright © 2020 Amory Rouault. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
